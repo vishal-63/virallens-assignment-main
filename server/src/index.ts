c4f3064ae809bab0173dce3082c404c9afbb6ea4
Vishal Shah <shahvishal910@gmail.com>
2025-11-21 14:39:52 +0530

    Simplfying loading envs on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 connectDB();
 
 app.use(helmet());
-console.log("|----Frontend URL: ", process.env.FRONTEND_URL);
+console.log("Frontend URL: ", process.env.FRONTEND_URL);
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
